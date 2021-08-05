15d81f46365d0a3f98b72deedf02edf10204dd76
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-08-05 17:42:13 +0700

    Initial refactoring of the lua source

--- interSubs.lua
+++ interSubs.lua
@@ -1,143 +1,125 @@
--- v. 2.7
--- Interactive subtitles for `mpv` for language learners.
---
--- default keybinding to start/stop: F5
--- default keybinding to hide/show: F6
--- if interSubs start automatically - mpv won't show notification
---
--- dirs in which interSubs will start automatically; part of path/filename will also work; case insensitive; regexp
--- autostart_in = {'German', ' ger ', '%.ger%.', 'Deutsch', 'Hebrew'}
-autostart_in = {'Hebrew'}
-
+local PLUGIN_TOGGLE_KEYBIND = "F5"
+local VISIBILITY_TOGGLE_KEYBIND = "F6"
+-- TODO: Probably needs to be changed. See utils.subprocess(t) and mp.command_native or mp.command_native_async
 -- for Mac change python3 to python or pythonw
-start_command = 'python3 "%s" "%s" "%s"'
+local PYTHON_COMMAND = 'python3 "%s" "%s" "%s"'
+local python_script_file = os.getenv("HOME").."/.config/mpv/scripts/interSubs.py"
 
--- recomend to have these in tmpfs, or at least ssd.
-sub_file = '/tmp/mpv_sub'
-mpv_socket = '/tmp/mpv_socket'
+local random_number = math.random(11111111, 99999999)
 
-keybinding = 'F5'
-keybinding_hide = 'F6'
-
-pyname = '~/.config/mpv/scripts/interSubs.py'
-
-------------------------------------------------------
-
-debug = false
--- debug = true
-
-if debug == true then
-	start_command = ''
-	start_command = 'terminator -e \'python3 "%s" "%s" "%s"; sleep 33\''
+local function create_mpv_socket_file()
+	-- recomend to have it in tmpfs
+	mpv_socket_file = "/tmp/mpv_socket"
+	mpv_socket_file = mpv_socket_file .. '_' .. random_number
+	mp.set_property("input-ipc-server", mpv_socket_file)
 end
 
-------------------------------------------------------
+local function create_subs_file()
+	-- recomend to have it in tmpfs
+	subs_file = "/tmp/mpv_sub"
+	subs_file =  subs_file .. '_' .. random_number
+end
 
-function s1()
-	if running == true then
-		s_rm()
-		return
-	end
+local function save_current_subs_settings()
+	sub_visibility = mp.get_property("sub-visibility")
+	sub_color = mp.get_property("sub-color", "1/1/1/1")
+	sub_border_color = mp.get_property("sub-border-color", "0/0/0/1")
+	sub_shadow_color = mp.get_property("sub-shadow-color", "0/0/0/1")
+end
 
-	running = true
-	mp.msg.warn('Starting interSubs ...')
-	mp.register_event("end-file", s_rm)
-	rnbr = math.random(11111111, 99999999)
+local function restore_subs_settings()
+	mp.set_property("sub-visibility", sub_visibility)
+	mp.set_property("sub-color", sub_color)
+	mp.set_property("sub-border-color", sub_border_color)
+	-- mp.set_property("sub-shadow-color", sub_shadow_color)
+end
 
-	if debug == true then
-		rnbr = ''
-	end
-
-	mpv_socket_2 = mpv_socket .. '_' .. rnbr
-	sub_file_2 =  sub_file .. '_' .. rnbr
-	
-	-- setting up socket to control mpv
-	mp.set_property("input-ipc-server", mpv_socket_2)
-	
-	-- without visible subs won't work
-	sbv = mp.get_property("sub-visibility")
-	mp.set_property("sub-visibility", "yes")
-	mp.set_property("sub-ass-override", "force")
-	
-	sub_color1 = mp.get_property("sub-color", "1/1/1/1")
-	sub_color2 = mp.get_property("sub-border-color", "0/0/0/1")
-	sub_color3 = mp.get_property("sub-shadow-color", "0/0/0/1")
+local function hide_native_subs()
 	mp.set_property("sub-color", "0/0/0/0")
 	mp.set_property("sub-border-color", "0/0/0/0")
 	mp.set_property("sub-shadow-color", "0/0/0/0")
-
-	start_command_2 = start_command:format(pyname:gsub('~', os.getenv('HOME')), mpv_socket_2, sub_file_2)
-	os.execute(start_command_2 .. ' &')
-
-	mp.observe_property("sub-text", "string", s2)
 end
 
-function s2(name, value)
+local function write_sub_to_file(name, value)
 	if type(value) == "string" then
-		file = io.open(sub_file_2, "w")
+		file = io.open(subs_file, "w")
 		file:write(value)
 		file:close()
 	end
 end
 
-function s_rm()
-	running = false
-	hidden = false
+local function release_resources()
+	-- TODO: needs refactoring
+	os.execute('pkill -f "' .. mpv_socket_file .. '" &')
+	os.execute('(sleep 3 && rm "' .. mpv_socket_file .. '") &')
+	os.execute('(sleep 3 && rm "' .. subs_file .. '") &')
+end
+
+local function start_intersub()
+	mp.command('show-text "Starting interSubs..."')
+	mp.msg.warn('Starting interSubs ...')
+
+	if mp.get_property("sub") == 'no' then
+		mp.command('show-text "Select subtitles before starting interSubs."')
+	end
+
+	mp.set_property("sub-visibility", "yes")
+	-- TODO: check if it needs to be like this
+	mp.set_property("sub-ass-override", "force")
+
+	save_current_subs_settings()
+	hide_native_subs()
+
+	create_mpv_socket_file()
+	create_subs_file()
+	mp.observe_property("sub-text", "string", write_sub_to_file)
+	
+	os.execute(PYTHON_COMMAND:format(python_script_file, mpv_socket_file, subs_file)..' &')
+	mp.register_event("end-file", stop_intersub)
+end
+
+local function stop_intersub()
+	mp.command('show-text "Quitting interSubs..."')
 	mp.msg.warn('Quitting interSubs ...')
 
-	mp.set_property("sub-visibility", sbv)
-	mp.set_property("sub-color", sub_color1)
-	mp.set_property("sub-border-color", sub_color2)
-	--~ mp.set_property("sub-shadow-color", sub_color3)
+	restore_subs_settings()
+	mp.unobserve_property(write_sub_to_file)
+	release_resources()
 
-	os.execute('pkill -f "' .. mpv_socket_2 .. '" &')
-	os.execute('(sleep 3 && rm "' .. mpv_socket_2 .. '") &')
-	os.execute('(sleep 3 && rm "' .. sub_file_2 .. '") &')
+	mp.unregister_event(stop_intersub)
 end
 
-function started()
-	if mp.get_property("sub") == 'no' then
-		return true
-	end
+local function on_file_loaded()
+	if mp.get_property("sub") == 'no' then return end
 	
-	hidden = false
-
-	for kk, pp in pairs(autostart_in) do
-		if mp.get_property("path"):lower():find(pp:lower()) or mp.get_property("working-directory"):lower():find(pp:lower()) then
-			s1()
-			break
-		end
-	end
+	run()
 end
 
-function s1_1()
-	if running == true then
-		s_rm()
-		mp.command('show-text "Quitting interSubs..."')
-	else
-		if mp.get_property("sub") == 'no' then
-			mp.command('show-text "Select subtitles before starting interSubs."')
-		else
-			s1()
-			mp.command('show-text "Starting interSubs..."')
-		end
-	end
-end
-
-function hide_show()
-	if running == true then
-		if hidden == true then
-			os.execute('rm "' .. mpv_socket_2 .. '_hide" &')
+local function toggle_subs_visibility()
+	-- TODO: doesn't look like it would work properly..., change it
+	if is_running then
+		if is_visible then
+			os.execute('rm "' .. mpv_socket_file .. '_hide" &')
 			mp.osd_message("Showing interSubs.", .8)
-			hidden = false
+			is_visible = false
 		else
-			os.execute('touch "' .. mpv_socket_2 .. '_hide" &')
+			os.execute('touch "' .. mpv_socket_file .. '_hide" &')
 			mp.osd_message("Hiding interSubs.", .8)
-			hidden = true
+			is_visible = true
 		end
 	end
 end
 
-mp.add_forced_key_binding(keybinding, "start-stop-interSubs", s1_1)
-mp.add_forced_key_binding(keybinding_hide, "hide-show-interSubs", hide_show)
-mp.register_event("file-loaded", started)
+local function run()
+	if is_running then
+		stop_intersub()
+		is_running = false
+	else
+		start_intersub()
+		is_running = true
+	end
+end
+
+mp.add_forced_key_binding(PLUGIN_TOGGLE_KEYBIND, "start-stop-interSubs", run)
+mp.add_forced_key_binding(VISIBILITY_TOGGLE_KEYBIND, "hide-show-interSubs", toggle_subs_visibility)
+mp.register_event("file-loaded", on_file_loaded)
