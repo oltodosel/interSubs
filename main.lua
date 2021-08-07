805a4a97978d64fa68196f218413e0b86573a994
alexgtd <49579984+alexgtd@users.noreply.github.com>
2021-08-07 13:24:38 +0700

    Refactor main start and stop functions
    Refactor start_intersub and stop_intersub functions, as well as related
    variables and functions.

--- main.lua
+++ main.lua
@@ -1,33 +1,46 @@
 local PLUGIN_TOGGLE_KEYBIND = "F5"
 local VISIBILITY_TOGGLE_KEYBIND = "F6"
--- TODO: Probably needs to be changed. See utils.subprocess(t) and mp.command_native or mp.command_native_async
 -- for Mac change python3 to python or pythonw
 local PYTHON_COMMAND = 'python3 "%s" "%s" "%s"'
-local python_script_file = os.getenv("HOME").."/.config/mpv/scripts/interSubs.py"
+local PYTHON_SCRIPT_FILENAME = "interSubs.py"
+
+local python_script_file_path = mp.get_script_directory().."/"..PYTHON_SCRIPT_FILENAME
 
 local random_number = math.random(11111111, 99999999)
 
-local function put_in_background(cmd) return cmd.." &" end
+local function put_cmd_in_bg(cmd) return cmd.." &" end
+
+local function is_there_no_selected_subs()
+	return mp.get_property("sub") == "no" or mp.get_property("sub") == "auto"
+end	
 
 local function create_mpv_socket_file()
 	-- recomend to have it in tmpfs
-	mpv_socket_file = "/tmp/mpv_socket"..'_'..random_number
-	mp.set_property("input-ipc-server", mpv_socket_file)
+	mpv_socket_file_path = "/tmp/mpv_socket"..'_'..random_number
+	mp.set_property("input-ipc-server", mpv_socket_file_path)
 end
 
 local function destroy_mpv_socket_file()
 	mp.set_property("input-ipc-server", "")
-	os.execute(put_in_background("rm "..mpv_socket_file))
+	os.execute(put_cmd_in_bg("rm "..mpv_socket_file_path))
+end
+
+local function write_sub_to_file(name, value)
+	if type(value) == "string" then
+		file = io.open(subs_file_path, "w")
+		file:write(value)
+		file:close()
+	end
 end
 
 local function create_subs_file()
 	-- recomend to have it in tmpfs
-	subs_file = "/tmp/mpv_sub"..'_'..random_number
+	subs_file_path = "/tmp/mpv_sub"..'_'..random_number
 	mp.observe_property("sub-text", "string", write_sub_to_file)
 end
 
 local function destroy_subs_file()
-	os.execute(put_in_background("rm "..subs_file))
+	os.execute(put_cmd_in_bg("rm "..subs_file_path))
 	mp.unobserve_property(write_sub_to_file)
 end
 
@@ -51,53 +64,37 @@ local function hide_native_subs()
 	mp.set_property("sub-shadow-color", "0/0/0/0")
 end
 
-local function write_sub_to_file(name, value)
-	if type(value) == "string" then
-		file = io.open(subs_file, "w")
-		file:write(value)
-		file:close()
-	end
-end
+local function stop_intersub()
+	mp.command('show-text "Quitting interSubs..."')
 
-local function release_resources()
-	-- TODO: needs refactoring
-	os.execute(put_in_background("pkill -f "..mpv_socket_file))
+	os.execute(put_cmd_in_bg("pkill -f "..mpv_socket_file_path))
 	destroy_mpv_socket_file()
 	destroy_subs_file()
+	restore_subs_settings()
+
+	mp.unregister_event(stop_intersub)
+	is_running = false
 end
 
 local function start_intersub()
-	mp.command('show-text "Starting interSubs..."')
-	mp.msg.warn('Starting interSubs ...')
-
-	if mp.get_property("sub") == 'no' then
+	if is_there_no_selected_subs() then
 		mp.command('show-text "Select subtitles before starting interSubs."')
+		return 
 	end
+	mp.command('show-text "Starting interSubs..."')
 
+	save_current_subs_settings()
 	mp.set_property("sub-visibility", "yes")
 	-- TODO: check if it needs to be like this
 	mp.set_property("sub-ass-override", "force")
-
-	save_current_subs_settings()
 	hide_native_subs()
 
-	create_mpv_socket_file()
 	create_subs_file()
-	mp.observe_property("sub-text", "string", write_sub_to_file)
-	
-	os.execute(PYTHON_COMMAND:format(python_script_file, mpv_socket_file, subs_file)..' &')
-	mp.register_event("end-file", stop_intersub)
-end
+	create_mpv_socket_file()
 
-local function stop_intersub()
-	mp.command('show-text "Quitting interSubs..."')
-	mp.msg.warn('Quitting interSubs ...')
-
-	restore_subs_settings()
-	mp.unobserve_property(write_sub_to_file)
-	release_resources()
-
-	mp.unregister_event(stop_intersub)
+	os.execute(put_cmd_in_bg(PYTHON_COMMAND:format(python_script_file_path, mpv_socket_file_path, subs_file_path)))
+	mp.register_event("shutdown", stop_intersub)
+	is_running = true
 end
 
 local function on_file_loaded()
@@ -124,10 +121,8 @@ end
 local function run()
 	if is_running then
 		stop_intersub()
-		is_running = false
 	else
 		start_intersub()
-		is_running = true
 	end
 end
 
